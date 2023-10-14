c09a172fabf0c48224ce2a225b8ae17b06787976
dank074 <torresefrain10@gmail.com>
2023-10-14 14:34:53 -0500

    close #33: support custom max_bitrate option

--- src/client/StreamOpts.ts
+++ src/client/StreamOpts.ts
@@ -3,6 +3,7 @@ export interface StreamOpts {
     height?: number;
     fps?: number;
     bitrateKbps?: number;
+    maxBitrateKbps?: number;
     hardware_acceleration?: boolean;
     video_codec?: 'H264' | 'VP8';
 }
@@ -12,6 +13,7 @@ export const streamOpts: StreamOpts = {
     height: 720,
     fps: 30,
     bitrateKbps: 1000,
+    maxBitrateKbps: 2500,
     hardware_acceleration: false,
     video_codec: 'H264'
 }
@@ -21,6 +23,7 @@ export const setStreamOpts = (opts: StreamOpts) => {
     streamOpts.height = opts.height ?? streamOpts.height;
     streamOpts.fps = opts.fps ?? streamOpts.fps;
     streamOpts.bitrateKbps = opts.bitrateKbps ?? streamOpts.bitrateKbps;
+    streamOpts.maxBitrateKbps = opts.maxBitrateKbps ?? streamOpts.maxBitrateKbps;
     streamOpts.hardware_acceleration = opts.hardware_acceleration ?? streamOpts.hardware_acceleration;
     streamOpts.video_codec = opts.video_codec ?? streamOpts.video_codec;
 }

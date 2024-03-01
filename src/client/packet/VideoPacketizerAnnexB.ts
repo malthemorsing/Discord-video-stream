e459e58787ad11ef1ee99a29ca9ed08288bc326f
Long Nguyen <nguyen.long.908132@gmail.com>
2024-03-02 00:18:54 +0700

    Change H265 `payloadType` to 0x67

--- src/client/packet/VideoPacketizerAnnexB.ts
+++ src/client/packet/VideoPacketizerAnnexB.ts
@@ -59,8 +59,8 @@ import {
 class VideoPacketizerAnnexB extends BaseMediaPacketizer {
     protected _nalFunctions: AnnexBHelpers;
 
-    constructor(connection: MediaUdp) {
-        super(connection, 0x65, true);
+    constructor(connection: MediaUdp, payloadType: number) {
+        super(connection, payloadType, true);
         this.srInterval = 5 * streamOpts.fps * 3; // ~5 seconds, assuming ~3 packets per frame
     }
 
@@ -159,7 +159,7 @@ class VideoPacketizerAnnexB extends BaseMediaPacketizer {
 
 export class VideoPacketizerH264 extends VideoPacketizerAnnexB {
     constructor(connection: MediaUdp) {
-        super(connection);
+        super(connection, 0x65);
         this._nalFunctions = H264Helpers;
     }
     /**
@@ -216,7 +216,7 @@ export class VideoPacketizerH264 extends VideoPacketizerAnnexB {
 
 export class VideoPacketizerH265 extends VideoPacketizerAnnexB {
     constructor(connection: MediaUdp) {
-        super(connection);
+        super(connection, 0x67);
         this._nalFunctions = H265Helpers;
     }
     /**
